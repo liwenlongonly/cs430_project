ad2dfa4382bc62e6c23d66dce58bc1b566db70a6
ilong <liwenlong01@inke.cn>
2020-07-27 16:56:43 +0800

    [MOD]格式代码

--- median_finding.py
+++ median_finding.py
@@ -8,7 +8,6 @@ import random
 from common import print_execute_time
 
 
-@print_execute_time
 def median_finding_randomized(a, p, r, i):
     """
     :param a: input array
@@ -16,10 +15,20 @@ def median_finding_randomized(a, p, r, i):
     :param r: the right edge of the array ...len(array) - 1]
     :param i: i-th smallest number, value range starts from 1
     """
-    return _median_finding_randomized(a, p, r, i)
+    if a is None or len(a) <= 0:
+        return -1
+    if p == r:
+        return a[p]
+    q = _randomized_partition(a, p, r)
+    k = q - p + 1
+    if i == k:
+        return a[q]
+    elif i < k:
+        return median_finding_randomized(a, p, q - 1, i)
+    else:
+        return median_finding_randomized(a, q + 1, r, i - k)
 
 
-@print_execute_time
 def median_finding_sort(a, p, r, i):
     """
     :param a: input array
@@ -53,21 +62,6 @@ def _randomized_partition(a, p, r):
     return _partition(a, p, r)
 
 
-def _median_finding_randomized(a, p, r, i):
-    if a is None or len(a) <= 0:
-        return -1
-    if p == r:
-        return a[p]
-    q = _randomized_partition(a, p, r)
-    k = q - p + 1
-    if i == k:
-        return a[q]
-    elif i < k:
-        return _median_finding_randomized(a, p, q - 1, i)
-    else:
-        return _median_finding_randomized(a, q + 1, r, i - k)
-
-
 def _merge_sort(a):
     n = len(a)
     if n <= 1:
@@ -93,29 +87,41 @@ def _merge_sort(a):
     return result
 
 
-def _test(a, median_finding):
-    print(a)
-    # print(median_finding(a, 0, len(a) - 1, 1))  # min
-    print(median_finding(a, 0, len(a) - 1, len(a)//2))  # median
-    # print(median_finding(a, 0, len(a) - 1, len(a)))  # max
-
-
 if __name__ == '__main__':
     from test_data import *
+
+
+    def _test(a, median_finding):
+        print(a)
+        # print(median_finding(a, 0, len(a) - 1, 1))  # min
+        print(median_finding(a, 0, len(a) - 1, len(a) // 2))  # median
+        # print(median_finding(a, 0, len(a) - 1, len(a)))  # max
+
+
+    @print_execute_time
+    def _median_finding_randomized_test(a, p, r, i):
+        return median_finding_randomized(a, p, r, i)
+
+
+    @print_execute_time
+    def _median_finding_sort_test(a, p, r, i):
+        return median_finding_sort(a, p, r, i)
+
+
     print("median_finding_randomized >>>>>>>>>>>>>>>")
     testArr1 = positive_sort_1k[:]
     testArr2 = reverse_sort_1k[:]
     testArr3 = random_1k[:]
-    _test(testArr1, median_finding_randomized)
-    _test(testArr2, median_finding_randomized)
-    _test(testArr3, median_finding_randomized)
+    _test(testArr1, _median_finding_randomized_test)
+    _test(testArr2, _median_finding_randomized_test)
+    _test(testArr3, _median_finding_randomized_test)
     print("median_finding_sort >>>>>>>>>>>>>>>")
     testArr1 = positive_sort_1k[:]
     testArr2 = reverse_sort_1k[:]
     testArr3 = random_1k[:]
-    _test(testArr1, median_finding_sort)
-    _test(testArr2, median_finding_sort)
-    _test(testArr3, median_finding_sort)
+    _test(testArr1, _median_finding_sort_test)
+    _test(testArr2, _median_finding_sort_test)
+    _test(testArr3, _median_finding_sort_test)
     print("median_finding_randomized >>>>>>>>>>>>>>>")
     testArr4 = random_1k[:]
     testArr5 = random_5k[:]
@@ -123,12 +129,12 @@ if __name__ == '__main__':
     testArr7 = random_15k[:]
     testArr8 = random_20k[:]
     testArr9 = random_25k[:]
-    _test(testArr4, median_finding_randomized)
-    _test(testArr5, median_finding_randomized)
-    _test(testArr6, median_finding_randomized)
-    _test(testArr7, median_finding_randomized)
-    _test(testArr8, median_finding_randomized)
-    _test(testArr9, median_finding_randomized)
+    _test(testArr4, _median_finding_randomized_test)
+    _test(testArr5, _median_finding_randomized_test)
+    _test(testArr6, _median_finding_randomized_test)
+    _test(testArr7, _median_finding_randomized_test)
+    _test(testArr8, _median_finding_randomized_test)
+    _test(testArr9, _median_finding_randomized_test)
     print("median_finding_sort >>>>>>>>>>>>>>>")
     testArr4 = random_1k[:]
     testArr5 = random_5k[:]
@@ -136,9 +142,9 @@ if __name__ == '__main__':
     testArr7 = random_15k[:]
     testArr8 = random_20k[:]
     testArr9 = random_25k[:]
-    _test(testArr4, median_finding_sort)
-    _test(testArr5, median_finding_sort)
-    _test(testArr6, median_finding_sort)
-    _test(testArr7, median_finding_sort)
-    _test(testArr8, median_finding_sort)
-    _test(testArr9, median_finding_sort)
+    _test(testArr4, _median_finding_sort_test)
+    _test(testArr5, _median_finding_sort_test)
+    _test(testArr6, _median_finding_sort_test)
+    _test(testArr7, _median_finding_sort_test)
+    _test(testArr8, _median_finding_sort_test)
+    _test(testArr9, _median_finding_sort_test)
--- quick_sort.py
+++ quick_sort.py
@@ -1,69 +1,105 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*- 
 import random
-from median_finding import median_finding_randomized 
-import sys
-sys.setrecursionlimit(10000000)
-class quick_sort:
+from median_finding import median_finding_randomized
+from common import print_execute_time
 
-	def main(self,n,type_="random"):
-		self.n=n
-		self.type_=type_
-		self.quicksort(self.n,0,len(n)-1)
-		return self.n	
 
-	def partition(self,n,p,r):
-		# random 代表使用随机数来进行快速排序
-		# median 代表使用中值来进行快速排序
-		# 不传   代表使用数组最后一个值的正常快速排序
-		
-		if str(self.type_)=="random":
-			random_x=random.randint(p,r-1)
-			temp=n[random_x]
-			n[random_x]=n[r]
-			n[r]=temp
-		if str(self.type_)=="median":
-			
-			n[r]=median_finding_randomized(n,p,r,int((r-p)/2+1))
-		
-		x=n[r]
-		i=p-1
-		for j in range(p,r):
- 
-			if n[j]<=x:
-				i+=1
-				temp=n[i]
-				n[i]=n[j]
-				n[j]=temp
+class QuickSort:
 
-		temp=n[i+1]
-		n[i+1]=n[r]
-		n[r]=temp
-		return i+1
+    def quickSort(self, n, type="random"):
+        self.n = n
+        self.type_ = type
+        self._quickSort(self.n, 0, len(n) - 1)
+        return self.n
 
-	def quicksort(self,n,p,r):
-		if p<r:
-			q=self.partition(n,p,r)
-			self.quicksort(n,p,q-1)
-			self.quicksort(n,q+1,r)
-	
+    def partition(self, n, p, r):
+        # random 代表使用随机数来进行快速排序
+        # median 代表使用中值来进行快速排序
+        # 不传   代表使用数组最后一个值的正常快速排序
+
+        if str(self.type_) == "random":
+            random_x = random.randint(p, r - 1)
+            temp = n[random_x]
+            n[random_x] = n[r]
+            n[r] = temp
+        if str(self.type_) == "median":
+            n[r] = median_finding_randomized(n, p, r, int((r - p) / 2 + 1))
+
+        x = n[r]
+        i = p - 1
+        for j in range(p, r):
+
+            if n[j] <= x:
+                i += 1
+                temp = n[i]
+                n[i] = n[j]
+                n[j] = temp
+
+        temp = n[i + 1]
+        n[i + 1] = n[r]
+        n[r] = temp
+        return i + 1
+
+    def _quickSort(self, n, p, r):
+        if p < r:
+            q = self.partition(n, p, r)
+            self._quickSort(n, p, q - 1)
+            self._quickSort(n, q + 1, r)
 
 
 if __name__ == '__main__':
+    from test_data import *
 
-	num=5000
-	n=[]
-	for x in range(0,num):
-		n.append(random.randint(0,x))
 
-	obj_=quick_sort()
-	#a=time.time()
-	obj_.main(n,'median')#带中值快排
-	#b=time.time()
-	obj_.main(n,'random')#带随机数快排
-	#c=time.time()
-	obj_.main(n,'')#直接快排
-	# d=time.time()
-	# print(b-a)
-	# print(c-b)
-	# print(d-c)	
+    @print_execute_time
+    def _quick_sort_test(n, type="random"):
+        obj_ = QuickSort()
+        return obj_.quickSort(n, type)
+
+
+    # print("quick sort random >>>>>>>>>>>>>>>")
+    # testArr1 = positive_sort_1k[:]
+    # testArr2 = reverse_sort_1k[:]
+    # testArr3 = random_1k[:]
+    # print(_quick_sort_test(testArr1, "random"))
+    # print(_quick_sort_test(testArr2, "random"))
+    # print(_quick_sort_test(testArr3, "random"))
+
+    # print("quick sort median >>>>>>>>>>>>>>>")
+    # testArr1 = positive_sort_1k[:]
+    # testArr2 = reverse_sort_1k[:]
+    # testArr3 = random_1k[:]
+    # print(_quick_sort_test(testArr1, "median"))
+    # print(_quick_sort_test(testArr2, "median"))
+    # print(_quick_sort_test(testArr3, "median"))
+
+    print("quick sort random >>>>>>>>>>>>>>>")
+    testArr4 = random_1k[:]
+    testArr5 = random_5k[:]
+    testArr6 = random_10k[:]
+    testArr7 = random_15k[:]
+    testArr8 = random_20k[:]
+    testArr9 = random_25k[:]
+    # print(_quick_sort_test(testArr4, "random"))
+    # print(_quick_sort_test(testArr5, "random"))
+    # print(_quick_sort_test(testArr6, "random"))
+    # print(_quick_sort_test(testArr7, "random"))
+    # print(_quick_sort_test(testArr8, "random"))
+    print(testArr9)
+    print(_quick_sort_test(testArr9, "random"))
+
+    print("quick sort median >>>>>>>>>>>>>>>")
+    testArr4 = random_1k[:]
+    testArr5 = random_5k[:]
+    testArr6 = random_10k[:]
+    testArr7 = random_15k[:]
+    testArr8 = random_20k[:]
+    testArr9 = random_25k[:]
+    # print(_quick_sort_test(testArr4, "median"))
+    # print(_quick_sort_test(testArr5, "median"))
+    # print(_quick_sort_test(testArr6, "median"))
+    # print(_quick_sort_test(testArr7, "median"))
+    # print(_quick_sort_test(testArr8, "median"))
+    print(testArr9)
+    print(_quick_sort_test(testArr9, "median"))
