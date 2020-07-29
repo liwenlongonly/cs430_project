7b22738627cbdf7cb46574fed38afe2f6125486b
liuhao <58254451@qq.com>
2020-07-29 19:43:46 +0800

    单独取用中值快排的时间

--- quick_sort.py
+++ quick_sort.py
@@ -3,15 +3,22 @@
 import random
 from median_finding import median_finding_randomized
 from common import print_execute_time
+import time
+import sys
 
-
+sys.setrecursionlimit(1000000)
 class QuickSort:
 
+    def __init__(self):
+        self.t_median_=0
+        self.t_all=0
     def quickSort(self, n, type="random"):
+        tmp=time.time()
         self.n = n
         self.type_ = type
         self._quickSort(self.n, 0, len(n) - 1)
-        return self.n
+        self.t_all=time.time()-tmp
+        return self.n,self.t_all-self.t_median_
 
     def partition(self, n, p, r):
         # random 代表使用随机数来进行快速排序
@@ -37,11 +44,10 @@ class QuickSort:
             n[i + 1] = x
             n[r] = temp
             return i + 1
-
         if str(self.type_) == "median":
-            
+            t_median_=time.time()
             x = median_finding_randomized(n, p, r, int((r - p + 1) / 2))
-
+            self.t_median_+=time.time()-t_median_
             i = p - 1
             
             for j in range(p, r):
@@ -61,29 +67,34 @@ class QuickSort:
             self._quickSort(n, q + 1, r)
 
 
-@print_execute_time
+
+#@print_execute_time
 def quick_sort_test(n, type="random"):
     obj_ = QuickSort()
     return obj_.quickSort(n, type)
 
 
 if __name__ == '__main__':
+
+
+    
+
     from test_data import *
 
     print("quick sort random >>>>>>>>>>>>>>>")
     testArr1 = positive_sort_1k[:]
     testArr2 = reverse_sort_1k[:]
     testArr3 = random_1k[:]
-    quick_sort_test(testArr1, "random")
-    quick_sort_test(testArr2, "random")
+    #quick_sort_test(testArr1, "random")
+    #quick_sort_test(testArr2, "random")
     quick_sort_test(testArr3, "random")
 
     print("quick sort median >>>>>>>>>>>>>>>")
     testArr1 = positive_sort_1k[:]
     testArr2 = reverse_sort_1k[:]
     testArr3 = random_1k[:]
-    print(quick_sort_test(testArr1, "median"))
-    print(quick_sort_test(testArr2, "median"))
+    #print(quick_sort_test(testArr1, "median"))
+    #print(quick_sort_test(testArr2, "median"))
     print(quick_sort_test(testArr3, "median"))
 
     print("quick sort random >>>>>>>>>>>>>>>")
